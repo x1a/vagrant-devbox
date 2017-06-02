a3e09c081a034c6d332e2eed84c0ede793269635
A1x <al.chechet@gmail.com>
2017-06-02 13:08:02 +0300

    set plugin = 'mysql_native_password' for root to allow other users use mysql -u root

--- vagrant/files/mysql/init.sql
+++ vagrant/files/mysql/init.sql
@@ -3,3 +3,5 @@ CREATE DATABASE testdb;
 CREATE DATABASE vagrant;
 CREATE USER `vagrant`@`localhost` IDENTIFIED BY 'secret';
 GRANT ALL ON vagrant.* TO `vagrant`@`localhost`;
+
+UPDATE `mysql`.`user` SET plugin = 'mysql_native_password' WHERE `User` = 'root';
